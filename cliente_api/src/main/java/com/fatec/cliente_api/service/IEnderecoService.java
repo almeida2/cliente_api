7d64a9fd832be0a5bf56420b8c4503ee7dc5ae65
almeida2 <projetodidaticovl@gmail.com>
2026-03-03 18:46:55 -0300

    documentacao - de classes e ct

--- cliente_api/src/main/java/com/fatec/cliente_api/service/IEnderecoService.java
+++ cliente_api/src/main/java/com/fatec/cliente_api/service/IEnderecoService.java
@@ -4,6 +4,12 @@ import java.util.Optional;
 
 import com.fatec.cliente_api.model.Endereco;
 
+/**
+ * Interface para obter o endereco da api ViaCEP
+ * 
+ * @param cep
+ * @return o json da api ViaCEP
+ */
 public interface IEnderecoService {
     public Optional<Endereco> obtemLogradouroPorCep(String cep);
 
